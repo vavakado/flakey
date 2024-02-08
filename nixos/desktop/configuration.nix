92fec72123abb2d4a59fc3009db560a43d23073f
vavakado <vavakado@skiff.com>
2024-02-09 00:10:47 +0200

    cleaned up desktop

--- nixos/desktop/configuration.nix
+++ nixos/desktop/configuration.nix
@@ -9,11 +9,10 @@
     ./hardware-configuration.nix
   ];
 
+  # enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Use the systemd-boot EFI boot loader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
+  # grub
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;
@@ -27,22 +26,27 @@
     };
   };
 
+  # my windows11(tm) drive
   fileSystems."/home/vavakado/heh" = {
     device = "/dev/disk/by-partuuid/2be7643d-46e7-96e2-d242-f873181ab5b4";
     fsType = "ntfs";
   };
-programs.steam.enable = true;
-  networking.hostName = "nixpc"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable =
-    true; # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
+  # gayming
+  programs.steam.enable = true;
+
+  networking.hostName = "nixpc"; # Define your hostname.
+
+  # wifi has never been so easy
+  networking.networkmanager.enable = true;
+
+  # yes
   time.timeZone = "Asia/Jerusalem";
 
+  # i am not a copyleft snob
   nixpkgs.config.allowUnfree = true;
 
+  # nvidia
   hardware.opengl = {
     enable = true;
     driSupport = true;
@@ -56,43 +60,21 @@ programs.steam.enable = true;
     forceFullCompositionPipeline = true;
   };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
+  # the goat
   i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
 
-  # Enable the X11 windowing system.
+  # i would love to use wayland but herbsluftwm is way too amazing
   services.xserver = {
     enable = true;
     windowManager.herbstluftwm.enable = true;
     displayManager.startx.enable = true;
+    libinput.mouse.accelProfile = "flat";
+    libinput.enable = true;
+    wacom.enable = true;
+    digimend.enable = true;
   };
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  programs.nix-ld.enable = true;
-
-  programs.nix-ld.libraries = with pkgs;
-    [
-
-      # Add any missing dynamic libraries for unpackaged programs
-
-      # here, NOT in environment.systemPackages
-
-    ];
-
-  # Enable sound.
+  # Enable pipewire (pisswire)
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -101,23 +83,28 @@ programs.steam.enable = true;
     pulse.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # i guess you've seen my username
   users.users.vavakado = {
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [ ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # dunno
+  programs.dconf.enable = true;
+  programs.gnupg = {
+    agent = {
+      enable = true;
+      pinentryFlavor = "gtk2";
+    };
+  };
+
+  # PACKAGES
   environment.systemPackages = with pkgs; [
     alacritty
+    anki
     btop
     calibre
-    ffmpeg
     clang
     cmake
     conda
@@ -127,6 +114,7 @@ programs.steam.enable = true;
     eza
     fd
     feh
+    ffmpeg
     flameshot
     gh
     git
@@ -170,13 +158,18 @@ programs.steam.enable = true;
     yuzu-early-access
     zoxide
   ];
-  virtualisation.docker.enableNvidia = true;
-  virtualisation.docker.enable = true;
-  virtualisation.docker.rootless.enable = true;
-  services.xserver.wacom.enable = true;
-  services.xserver.digimend.enable = true;
+
+  # docker
+  virtualisation.docker = {
+    enableNvidia = true;
+    enable = true;
+    rootless.enable = true;
+  };
+
+  # so calibre can see my book
   services.udisks2.enable = true;
 
+  # i still can't decide between these three
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk
@@ -185,46 +178,10 @@ programs.steam.enable = true;
       fonts = [ "CascadiaCode" "Iosevka" "JetBrainsMono" ];
     })
   ];
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+  # Enable the sshd
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11"; # DO NOT CHANGE
 
 }
