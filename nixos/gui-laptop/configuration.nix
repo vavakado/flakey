034af7203e6b3aa869b935d42e3b0cc9ace5fe74
Vladimir Rubin <vavakado@proton.me>
2024-07-01 01:59:12 +0300

    minecraft

--- nixos/gui-laptop/configuration.nix
+++ nixos/gui-laptop/configuration.nix
@@ -108,23 +108,76 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Pantheon Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  programs.hyprland.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  #  services.xserver.desktopManager.pantheon.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.vavakado = {
     isNormalUser = true;
     description = "Vladimir Rubin";
     extraGroups = [ "networkmanager" "docker" "wheel" ];
+    packages = with pkgs;
+      [
+        #  thunderbird
+      ];
   };
 
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
      wget
      neovim
      tmux
      docker-compose
+     firefox
+     bemenu
+     foot
      git
+     telegram-desktop
+     libinput
      htop
   ];
 
--- nixos/gui-laptop/home.nix
+++ nixos/gui-laptop/home.nix
@@ -16,6 +16,22 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
     git
     gnumake
     unzip
@@ -23,12 +39,19 @@
     gcc
     ripgrep
     wl-clipboard
+    imv
+    neovide
+    nb
     luajitPackages.luacheck
     lua-language-server
     stylua
     eza
     btop
     gh
+    keepassxc
+    wallust
+    swww
+    flavours
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
